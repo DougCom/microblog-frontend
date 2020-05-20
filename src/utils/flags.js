d71f023b015f86ae44f8378012dd935768544a34
Doug Tidwell <doug@dougtidwell.com>
2020-05-20 19:38:40 -0400

    Added new flag

--- src/utils/flags.js
+++ src/utils/flags.js
@@ -3,10 +3,10 @@ import Rox from 'rox-browser'
 export const Flags = {
   sidebar: new Rox.Flag(false),
   title: new Rox.Flag(false)
-};
+}
 
 const options = {
-};
+}
 
-Rox.register('default', Flags);
-Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options);
+Rox.register('default', Flags)
+Rox.setup(process.env.VUE_APP_ROLLOUT_KEY, options)
